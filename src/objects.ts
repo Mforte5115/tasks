b54f66b302312b5d94d03d0fbc243886c4a4d0ad
mforte5115 <mforte@udel.edu>
2024-02-20 22:34:14 -0500

    toMarkdown finished.

--- src/objects.ts
+++ src/objects.ts
@@ -81,7 +81,12 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    const markDown: string = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        const markDownMult = markDown + "\n- " + question.options.join("\n- ");
+        return markDownMult;
+    }
+    return markDown;
 }
 
 /**
