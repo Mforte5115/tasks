86f82aca9423a91459f9c7b8c637656430358877
mforte5115 <mforte@udel.edu>
2024-02-29 01:11:32 -0500

    Finished task.

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,49 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "Halloween"
+    | "Christmas"
+    | "Thanksgiving"
+    | "July4th"
+    | "Easter";
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("Easter");
+
+    const alphaHoliday: Record<Holiday, Holiday> = {
+        Christmas: "Easter",
+        Easter: "Halloween",
+        Halloween: "July4th",
+        July4th: "Thanksgiving",
+        Thanksgiving: "Christmas"
+    };
+    const timeHoliday: Record<Holiday, Holiday> = {
+        Easter: "July4th",
+        July4th: "Halloween",
+        Halloween: "Thanksgiving",
+        Thanksgiving: "Christmas",
+        Christmas: "Easter"
+    };
+    const holidayEmojis: Record<Holiday, string> = {
+        Easter: "🐇",
+        July4th: "🎆",
+        Halloween: "👻",
+        Thanksgiving: "🦃",
+        Christmas: "🎄"
+    };
+
+    return (
+        <div>
+            <div>
+                <Button onClick={() => setHoliday(alphaHoliday[holiday])}>
+                    Advance by Alphabet
+                </Button>
+                <Button onClick={() => setHoliday(timeHoliday[holiday])}>
+                    Advance by Year
+                </Button>
+            </div>
+            <div>Holiday: {holidayEmojis[holiday]}</div>
+        </div>
+    );
 }
